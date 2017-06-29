67d292d62e0c39062d8c734276308cac6a4e70ce
Augusto <dev.augusto@gmail.com>
2017-06-29 18:39:16 -0300

    Initial Commit

---
+++ learning-head-first-c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  learning-head-first-c
+//
+//  Created by Augusto Cesar on 29/06/17.
+//  Copyright © 2017 Augusto Cesar. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
